957fbc76d41b3181b14ed80efab716902c9299d8
fry <fry.land@hotmail.fr>
2022-07-26 15:37:53 +0200

    Data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
+import ArticleList from '../components/ArticleList';
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div>
       {/* Put Head tag to add head tag in HTML. title, metatag... */}
@@ -8,7 +9,18 @@ export default function Home() {
         <title>WebDev News</title>
         <meta name='keywords' content='web development, programming, NextJS' />
       </Head>
-      <h1>Welcome to Next</h1>
+      <ArticleList articles={articles} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/posts?_limit=6'
+  );
+  const articles = await res.json();
+
+  return {
+    props: { articles },
+  };
+};
